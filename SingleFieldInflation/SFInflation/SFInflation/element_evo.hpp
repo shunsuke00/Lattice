7a238fce293cab65c4a0837c86e45ec1211fc378
shunshun <134624826+shunsuke00@users.noreply.github.com>
2025-01-27 12:38:36 +0900

    Add files via upload

---
+++ SingleFieldInflation/SFInflation/SFInflation/element_evo.hpp
@@ -0,0 +1,207 @@
+#ifndef _ELEMENT_EVO_
+#define _ELEMENT_EVO_
+
+#include "latticeeasy.hpp"
+
+/* Heavily used functions */
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------*/
+inline int INCREMENT(int i)
+{
+  return( (i==N-1) ? 0 : i+1 );
+}
+
+inline int DECREMENT(int i)
+{
+  return( (i==0) ? N-1 : i-1 );
+}
+
+
+/* Laplacian */
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------*/
+inline double lapl(int fld, int i, int j, int k, int kloop_arg, FSUB_ARG)
+{
+  double value;
+
+  switch(kloop_arg)
+  {
+    case 1:
+      value = (f[fld][i][j][k+1] + f[fld][i][j][k-1]
+            +f[fld][i][j+1][k] + f[fld][i][j-1][k]
+            +f[fld][i+1][j][k] + f[fld][i-1][j][k]
+            -6.*f[fld][i][j][k]);
+      break;
+    case 2:
+    case 4:
+      value = (fsub1[fld][i][j][k+1] + fsub1[fld][i][j][k-1]
+            +fsub1[fld][i][j+1][k] + fsub1[fld][i][j-1][k]
+            +fsub1[fld][i+1][j][k] + fsub1[fld][i-1][j][k]
+            -6.*fsub1[fld][i][j][k]);
+      break;
+    case 3:
+      value = (fsub2[fld][i][j][k+1] + fsub2[fld][i][j][k-1]
+            +fsub2[fld][i][j+1][k] + fsub2[fld][i][j-1][k]
+            +fsub2[fld][i+1][j][k] + fsub2[fld][i-1][j][k]
+            -6.*fsub2[fld][i][j][k]);
+      break;
+  }
+
+  return value;
+}
+
+
+inline double laplb(int fld, int i, int j, int k, int kloop_arg, FSUB_ARG)
+{
+  double value;
+
+  switch(kloop_arg)
+  {
+    case 1:
+      value = (f[fld][i][j][INCREMENT(k)] + f[fld][i][j][DECREMENT(k)]
+            +f[fld][i][INCREMENT(j)][k] + f[fld][i][DECREMENT(j)][k]
+            +f[fld][INCREMENT(i)][j][k] + f[fld][DECREMENT(i)][j][k]
+            -6.*f[fld][i][j][k]);
+      break;
+    case 2:
+    case 4:
+      value = (fsub1[fld][i][j][INCREMENT(k)] + fsub1[fld][i][j][DECREMENT(k)]
+            +fsub1[fld][i][INCREMENT(j)][k] + fsub1[fld][i][DECREMENT(j)][k]
+            +fsub1[fld][INCREMENT(i)][j][k] + fsub1[fld][DECREMENT(i)][j][k]
+            -6.*fsub1[fld][i][j][k]);
+      break;
+    case 3:
+      value = (fsub2[fld][i][j][INCREMENT(k)] + fsub2[fld][i][j][DECREMENT(k)]
+            +fsub2[fld][i][INCREMENT(j)][k] + fsub2[fld][i][DECREMENT(j)][k]
+            +fsub2[fld][INCREMENT(i)][j][k] + fsub2[fld][DECREMENT(i)][j][k]
+            -6.*fsub2[fld][i][j][k]);
+      break;
+  }
+
+  return value;
+}
+
+
+/* Gradient energy (externally called) 1/2 <|Grad(f)|^2> = 1/2 <-f Lapl(f)> */
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------*/
+double gradient_energy(int fld, int kloop_arg, FSUB_ARG)
+{
+  int i=0,j=0,k=0;
+  double gradient=0.;
+  double norm=1./pw2(dx); // Converts lapl() to actual Laplacian
+
+  switch(kloop_arg)
+  {
+    case 1:
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j,k)
+      for(i=1;i<N-1;i++)
+        for(j=1;j<N-1;j++)
+          for(k=1;k<N-1;k++)
+          {
+            gradient -= FIELD(fld)*lapl(fld,i,j,k,kloop_arg,FSUB);
+          }
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j)
+      for(i=0;i<N;i++)
+        for(j=0;j<N;j++)
+        {
+          gradient -= f[fld][i][j][0]*laplb(fld,i,j,0,kloop_arg,FSUB) + f[fld][i][j][N-1]*laplb(fld,i,j,N-1,kloop_arg,FSUB); // z=0,N-1
+          if(j==0 || j==N-1) continue;
+          gradient -= f[fld][i][0][j]*laplb(fld,i,0,j,kloop_arg,FSUB) + f[fld][i][N-1][j]*laplb(fld,i,N-1,j,kloop_arg,FSUB); // y=0,N-1
+          if(i==0 || i==N-1) continue;
+          gradient -= f[fld][0][i][j]*laplb(fld,0,i,j,kloop_arg,FSUB) + f[fld][N-1][i][j]*laplb(fld,N-1,i,j,kloop_arg,FSUB); // x=0,N-1
+        }
+      break;
+    case 2:
+    case 4:
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j,k)
+      for(i=1;i<N-1;i++)
+        for(j=1;j<N-1;j++)
+          for(k=1;k<N-1;k++)
+          {
+            gradient -= FIELDSUB1(fld)*lapl(fld,i,j,k,kloop_arg,FSUB);
+          }
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j)
+      for(i=0;i<N;i++)
+        for(j=0;j<N;j++)
+        {
+          gradient -= fsub1[fld][i][j][0]*laplb(fld,i,j,0,kloop_arg,FSUB) + fsub1[fld][i][j][N-1]*laplb(fld,i,j,N-1,kloop_arg,FSUB); // z=0,N-1
+          if(j==0 || j==N-1) continue;
+          gradient -= fsub1[fld][i][0][j]*laplb(fld,i,0,j,kloop_arg,FSUB) + fsub1[fld][i][N-1][j]*laplb(fld,i,N-1,j,kloop_arg,FSUB); // y=0,N-1
+          if(i==0 || i==N-1) continue;
+          gradient -= fsub1[fld][0][i][j]*laplb(fld,0,i,j,kloop_arg,FSUB) + fsub1[fld][N-1][i][j]*laplb(fld,N-1,i,j,kloop_arg,FSUB); // x=0,N-1
+        }
+      break;
+    case 3:
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j,k)
+      for(i=1;i<N-1;i++)
+        for(j=1;j<N-1;j++)
+          for(k=1;k<N-1;k++)
+          {
+            gradient -= FIELDSUB2(fld)*lapl(fld,i,j,k,kloop_arg,FSUB);
+          }
+    #pragma omp parallel for reduction(-:gradient) num_threads(num_thread) private(j)    
+      for(i=0;i<N;i++)
+        for(j=0;j<N;j++)
+        {
+          gradient -= fsub2[fld][i][j][0]*laplb(fld,i,j,0,kloop_arg,FSUB) + fsub2[fld][i][j][N-1]*laplb(fld,i,j,N-1,kloop_arg,FSUB); // z=0,N-1
+          if(j==0 || j==N-1) continue;
+          gradient -= fsub2[fld][i][0][j]*laplb(fld,i,0,j,kloop_arg,FSUB) + fsub2[fld][i][N-1][j]*laplb(fld,i,N-1,j,kloop_arg,FSUB); // y=0,N-1
+          if(i==0 || i==N-1) continue;
+          gradient -= fsub2[fld][0][i][j]*laplb(fld,0,i,j,kloop_arg,FSUB) + fsub2[fld][N-1][i][j]*laplb(fld,N-1,i,j,kloop_arg,FSUB); // x=0,N-1
+        }
+  }
+
+  return(.5*gradient*norm/(double)gridsize);
+}
+
+
+/* rho-3p (used in ad evolution) */
+/*------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------*/
+double stress_3pressure(int kloop_arg, SUB_ARG)
+{
+  int i,j,k,fld;
+  double rho_3p=0.;
+
+  switch(kloop_arg)
+  {
+    case 1:
+    #pragma omp parallel for reduction(-:rho_3p) num_threads(num_thread) private(j,k)
+      LOOP
+      {
+        rho_3p -= pw2(FIELDD(0));
+      }
+      rho_3p /= (double)gridsize*pw2(a);
+      rho_3p += 2.*gradient_energy(0,kloop_arg,FSUB)/pow(a,2.*rescale_s+2.);
+      rho_3p += 4.*potential_energy(kloop_arg,FSUB);
+      break;
+    case 2:
+    case 4:
+    #pragma omp parallel for reduction(-:rho_3p) num_threads(num_thread) private(j,k)
+      LOOP
+      {
+        rho_3p -= pw2(FIELDDSUB1(0));
+      }
+      rho_3p /= (double)gridsize*pw2(asub1);
+      rho_3p += 2.*gradient_energy(0,kloop_arg,FSUB)/pow(asub1,2.*rescale_s+2.);
+      rho_3p += 4.*potential_energy(kloop_arg,FSUB);
+      break;
+    case 3:
+    #pragma omp parallel for reduction(-:rho_3p) num_threads(num_thread) private(j,k)
+      LOOP
+      {
+        rho_3p -= pw2(FIELDDSUB2(0));
+      }
+      rho_3p /= (double)gridsize*pw2(asub2);
+      rho_3p += 2.*gradient_energy(0,kloop_arg,FSUB)/pow(asub2,2.*rescale_s+2.);
+      rho_3p += 4.*potential_energy(kloop_arg,FSUB);
+      break;
+  }
+
+  return rho_3p;
+}
+
+
+
+#endif
---
+++ SingleFieldInflation/SFInflation/SFInflation/evolution.cpp
@@ -0,0 +1,306 @@
+#include "latticeeasy.hpp"
+#include "element_evo.hpp"
+
+
+void evolve_fields(double d, int kloop_arg, SUB_ARG, double kf[nflds][N][N][N])
+{
+  DECLARE_INDICES
+  int fld;
+
+  for(fld=0;fld<nflds;fld++)
+  {
+    switch(kloop_arg)
+    {
+      case 1:
+        #pragma omp parallel for num_threads(num_thread) private(j,k)
+        LOOP
+        {
+          KFIELD(fld) = d*FIELDD(fld)/6.;
+          FIELDSUB1(fld) = FIELD(fld) + d*FIELDD(fld)*0.5;
+        }
+        break;
+      case 2:
+        #pragma omp parallel for num_threads(num_thread) private(j,k)
+        LOOP
+        {
+          KFIELD(fld) += d*FIELDDSUB1(fld)/3.;
+          FIELDSUB2(fld) = FIELD(fld) + d*FIELDDSUB1(fld)*0.5;
+        }
+        break;
+      case 3:
+        #pragma omp parallel for num_threads(num_thread) private(j,k)
+        LOOP
+        {
+          KFIELD(fld) += d*FIELDDSUB2(fld)/3.;
+          FIELDSUB1(fld) = FIELD(fld) + d*FIELDDSUB2(fld);
+        }
+        break;
+      case 4:
+        #pragma omp parallel for num_threads(num_thread) private(j,k)
+        LOOP
+        {
+          KFIELD(fld) += d*FIELDDSUB1(fld)/6.;
+        }
+        break;
+    }
+  }
+}
+
+
+void evolve_derivs(double d, int kloop_arg, SUB_ARG, double kfd[nflds][N][N][N])
+{
+  int i=0,j=0,k=0,fld;
+  double laplnorm,acoeff,tempfd;
+
+
+  for(fld=0;fld<nflds;fld++)
+  {
+    switch(kloop_arg)
+    {
+      case 1:
+        laplnorm = 1./pw2(dx)/pow(a,2.*rescale_s);
+        acoeff = ad/a;
+        #pragma omp parallel for num_threads(num_thread) private(j,k,tempfd)
+        for(i=1;i<N-1;i++)
+          for(j=1;j<N-1;j++)
+            for(k=1;k<N-1;k++)
+            {
+              tempfd = d*(-(rescale_s+2.)*acoeff*FIELDD(fld) + laplnorm*lapl(fld,i,j,k,kloop_arg,FSUB) - pw2(a)*dvdf(fld,i,j,k,kloop_arg,FSUB));
+              KFIELDD(fld) = tempfd/6.;
+              FIELDDSUB1(fld) = FIELDD(fld) + tempfd*0.5;
+            }
+        #pragma omp parallel for num_threads(num_thread) private(j,tempfd)
+        for(i=0;i<N;i++)
+          for(j=0;j<N;j++)
+          {
+            // z=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][i][j][0] + laplnorm*laplb(fld,i,j,0,kloop_arg,FSUB) - pw2(a)*dvdf(fld,i,j,0,kloop_arg,FSUB));
+            kfd[fld][i][j][0] = tempfd/6.;
+            fdsub1[fld][i][j][0] = fd[fld][i][j][0] + tempfd*0.5;
+            // z=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][i][j][N-1] + laplnorm*laplb(fld,i,j,N-1,kloop_arg,FSUB) - pw2(a)*dvdf(fld,i,j,N-1,kloop_arg,FSUB));
+            kfd[fld][i][j][N-1] = tempfd/6.;
+            fdsub1[fld][i][j][N-1] = fd[fld][i][j][N-1] + tempfd*0.5;
+            if(j==0 || j==N-1) continue;
+            // y=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][i][0][j] + laplnorm*laplb(fld,i,0,j,kloop_arg,FSUB) - pw2(a)*dvdf(fld,i,0,j,kloop_arg,FSUB));
+            kfd[fld][i][0][j] = tempfd/6.;
+            fdsub1[fld][i][0][j] = fd[fld][i][0][j] + tempfd*0.5;
+            // y=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][i][N-1][j] + laplnorm*laplb(fld,i,N-1,j,kloop_arg,FSUB) - pw2(a)*dvdf(fld,i,N-1,j,kloop_arg,FSUB));
+            kfd[fld][i][N-1][j] = tempfd/6.;
+            fdsub1[fld][i][N-1][j] = fd[fld][i][N-1][j] + tempfd*0.5;
+            if(i==0 || i==N-1) continue;
+            // x=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][0][i][j] + laplnorm*laplb(fld,0,i,j,kloop_arg,FSUB) - pw2(a)*dvdf(fld,0,i,j,kloop_arg,FSUB));
+            kfd[fld][0][i][j] = tempfd/6.;
+            fdsub1[fld][0][i][j] = fd[fld][0][i][j] + tempfd*0.5;
+            // x=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fd[fld][N-1][i][j] + laplnorm*laplb(fld,N-1,i,j,kloop_arg,FSUB) - pw2(a)*dvdf(fld,N-1,i,j,kloop_arg,FSUB));
+            kfd[fld][N-1][i][j] = tempfd/6.;
+            fdsub1[fld][N-1][i][j] = fd[fld][N-1][i][j] + tempfd*0.5;
+          }
+        break;
+      case 2:
+        laplnorm = 1./pw2(dx)/pow(asub1,2.*rescale_s);
+        acoeff = adsub1/asub1;
+        #pragma omp parallel for num_threads(num_thread) private(j,k,tempfd)
+        for(i=1;i<N-1;i++)
+          for(j=1;j<N-1;j++)
+            for(k=1;k<N-1;k++)
+            {
+              tempfd = d*(-(rescale_s+2.)*acoeff*FIELDDSUB1(fld) + laplnorm*lapl(fld,i,j,k,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,k,kloop_arg,FSUB));
+              KFIELDD(fld) += tempfd/3.;
+              FIELDDSUB2(fld) = FIELDD(fld) + tempfd*0.5;
+            }
+        #pragma omp parallel for num_threads(num_thread) private(j,tempfd)
+        for(i=0;i<N;i++)
+          for(j=0;j<N;j++)
+          {
+            // z=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][j][0] + laplnorm*laplb(fld,i,j,0,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,0,kloop_arg,FSUB));
+            kfd[fld][i][j][0] += tempfd/3.;
+            fdsub2[fld][i][j][0] = fd[fld][i][j][0] + tempfd*0.5;
+            // z=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][j][N-1] + laplnorm*laplb(fld,i,j,N-1,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,N-1,kloop_arg,FSUB));
+            kfd[fld][i][j][N-1] += tempfd/3.;
+            fdsub2[fld][i][j][N-1] = fd[fld][i][j][N-1] + tempfd*0.5;
+            if(j==0 || j==N-1) continue;
+            // y=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][0][j] + laplnorm*laplb(fld,i,0,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,0,j,kloop_arg,FSUB));
+            kfd[fld][i][0][j] += tempfd/3.;
+            fdsub2[fld][i][0][j] = fd[fld][i][0][j] + tempfd*0.5;
+            // y=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][N-1][j] + laplnorm*laplb(fld,i,N-1,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,N-1,j,kloop_arg,FSUB));
+            kfd[fld][i][N-1][j] += tempfd/3.;
+            fdsub2[fld][i][N-1][j] = fd[fld][i][N-1][j] + tempfd*0.5;
+            if(i==0 || i==N-1) continue;
+            // x=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][0][i][j] + laplnorm*laplb(fld,0,i,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,0,i,j,kloop_arg,FSUB));
+            kfd[fld][0][i][j] += tempfd/3.;
+            fdsub2[fld][0][i][j] = fd[fld][0][i][j] + tempfd*0.5;
+            // x=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub1[fld][N-1][i][j] + laplnorm*laplb(fld,N-1,i,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,N-1,i,j,kloop_arg,FSUB));
+            kfd[fld][N-1][i][j] += tempfd/3.;
+            fdsub2[fld][N-1][i][j] = fd[fld][N-1][i][j] + tempfd*0.5;
+          }
+        break;
+      case 3:
+        laplnorm = 1./pw2(dx)/pow(asub2,2.*rescale_s);
+        acoeff = adsub2/asub2;
+        #pragma omp parallel for num_threads(num_thread) private(j,k,tempfd)
+        for(i=1;i<N-1;i++)
+          for(j=1;j<N-1;j++)
+            for(k=1;k<N-1;k++)
+            {
+              tempfd = d*(-(rescale_s+2.)*acoeff*FIELDDSUB2(fld) + laplnorm*lapl(fld,i,j,k,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,i,j,k,kloop_arg,FSUB));
+              KFIELDD(fld) += tempfd/3.;
+              FIELDDSUB1(fld) = FIELDD(fld) + tempfd;
+            }
+        #pragma omp parallel for num_threads(num_thread) private(j,tempfd)
+        for(i=0;i<N;i++)
+          for(j=0;j<N;j++)
+          {
+            // z=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][i][j][0] + laplnorm*laplb(fld,i,j,0,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,i,j,0,kloop_arg,FSUB));
+            kfd[fld][i][j][0] += tempfd/3.;
+            fdsub1[fld][i][j][0] = fd[fld][i][j][0] + tempfd;
+            // z=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][i][j][N-1] + laplnorm*laplb(fld,i,j,N-1,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,i,j,N-1,kloop_arg,FSUB));
+            kfd[fld][i][j][N-1] += tempfd/3.;
+            fdsub1[fld][i][j][N-1] = fd[fld][i][j][N-1] + tempfd;
+            if(j==0 || j==N-1) continue;
+            // y=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][i][0][j] + laplnorm*laplb(fld,i,0,j,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,i,0,j,kloop_arg,FSUB));
+            kfd[fld][i][0][j] += tempfd/3.;
+            fdsub1[fld][i][0][j] = fd[fld][i][0][j] + tempfd;
+            // y=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][i][N-1][j] + laplnorm*laplb(fld,i,N-1,j,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,i,N-1,j,kloop_arg,FSUB));
+            kfd[fld][i][N-1][j] += tempfd/3.;
+            fdsub1[fld][i][N-1][j] = fd[fld][i][N-1][j] + tempfd;
+            if(i==0 || i==N-1) continue;
+            // x=0
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][0][i][j] + laplnorm*laplb(fld,0,i,j,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,0,i,j,kloop_arg,FSUB));
+            kfd[fld][0][i][j] += tempfd/3.;
+            fdsub1[fld][0][i][j] = fd[fld][0][i][j] + tempfd;
+            // x=N-1
+            tempfd = d*(-(rescale_s+2.)*acoeff*fdsub2[fld][N-1][i][j] + laplnorm*laplb(fld,N-1,i,j,kloop_arg,FSUB) - pw2(asub2)*dvdf(fld,N-1,i,j,kloop_arg,FSUB));
+            kfd[fld][N-1][i][j] += tempfd/3.;
+            fdsub1[fld][N-1][i][j] = fd[fld][N-1][i][j] + tempfd;
+          }
+        break;
+      case 4:
+        laplnorm = 1./pw2(dx)/pow(asub1,2.*rescale_s);
+        acoeff = adsub1/asub1;
+        #pragma omp parallel for num_threads(num_thread) private(j,k,tempfd)
+        for(i=1;i<N-1;i++)
+          for(j=1;j<N-1;j++)
+            for(k=1;k<N-1;k++)
+            {
+              KFIELDD(fld) += d*(-(rescale_s+2.)*acoeff*FIELDDSUB1(fld) + laplnorm*lapl(fld,i,j,k,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,k,kloop_arg,FSUB))/6.;
+            }
+        #pragma omp parallel for num_threads(num_thread) private(j,k,tempfd)
+        for(i=0;i<N;i++)
+          for(j=0;j<N;j++)
+          {
+            // z=0
+            kfd[fld][i][j][0] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][j][0] + laplnorm*laplb(fld,i,j,0,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,0,kloop_arg,FSUB))/6.;
+            // z=N-1
+            kfd[fld][i][j][N-1] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][j][N-1] + laplnorm*laplb(fld,i,j,N-1,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,j,N-1,kloop_arg,FSUB))/6.;
+            if(j==0 || j==N-1) continue;
+            // y=0
+            kfd[fld][i][0][j] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][0][j] + laplnorm*laplb(fld,i,0,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,0,j,kloop_arg,FSUB))/6.;
+            // y=N-1
+            kfd[fld][i][N-1][j] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][i][N-1][j] + laplnorm*laplb(fld,i,N-1,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,i,N-1,j,kloop_arg,FSUB))/6.;
+            if(i==0 || i==N-1) continue;
+            // x=0
+            kfd[fld][0][i][j] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][0][i][j] + laplnorm*laplb(fld,0,i,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,0,i,j,kloop_arg,FSUB))/6.;
+            // x=N-1
+            kfd[fld][N-1][i][j] += d*(-(rescale_s+2.)*acoeff*fdsub1[fld][N-1][i][j] + laplnorm*laplb(fld,N-1,i,j,kloop_arg,FSUB) - pw2(asub1)*dvdf(fld,N-1,i,j,kloop_arg,FSUB))/6.;
+          }
+        break;
+    }
+  }
+}
+
+
+void evolve_scale(double d, int kloop_arg)
+{
+  switch(kloop_arg)
+  {
+    case 1:
+      ka = d*ad/6.;
+      asub1 = a + d*ad*0.5;
+      break;
+    case 2:
+      ka += d*adsub1/3.;
+      asub2 = a + d*adsub1*0.5;
+      break;
+    case 3:
+      ka += d*adsub2/3.;
+      asub1 = a + d*adsub2;
+      break;
+    case 4:
+      ka += d*adsub1/6.;
+      break;
+  }
+}
+
+
+
+void evolve_scaled(double d, int kloop_arg, SUB_ARG)
+{
+  double tempad;
+
+  switch(kloop_arg)
+  {
+    case 1:
+      tempad = d*(-rescale_s*pw2(ad)/a + pow(a,3.)*(stress_3pressure(kloop_arg,SUB))/(6.));
+      kad = tempad/6.;
+      adsub1 = ad + tempad*0.5;
+      break;
+    case 2:
+      tempad = d*(-rescale_s*pw2(adsub1)/asub1 + pow(asub1,3.)*(stress_3pressure(kloop_arg,SUB))/(6.));
+      kad += tempad/3.;
+      adsub2 = ad + tempad*0.5;
+      break;
+    case 3:
+      tempad = d*(-rescale_s*pw2(adsub2)/asub2 + pow(asub2,3.)*(stress_3pressure(kloop_arg,SUB))/(6.));
+      kad += tempad/3.;
+      adsub1 = ad + tempad;
+      break;
+    case 4:
+      kad += d*(-rescale_s*pw2(adsub1)/asub1 + pow(asub1,3.)*(stress_3pressure(kloop_arg,SUB))/(6.))/6.;
+      break;
+  }
+}
+
+
+void evolve_rk4(double dt, SUB_ARG, double kf[nflds][N][N][N], double kfd[nflds][N][N][N])
+{
+  DECLARE_INDICES
+  int kloop,fld;
+
+  /* calculate delta */
+  for(kloop=1;kloop<=4;kloop++)
+  {
+    evolve_fields(dt,kloop,SUB,kf);
+    evolve_derivs(dt,kloop,SUB,kfd);
+    evolve_scale (dt,kloop);
+    evolve_scaled(dt,kloop,SUB);
+  }
+
+  /* evolution */
+  for(fld=0;fld<nflds;fld++)
+  {
+    LOOP
+    {
+      FIELD(fld)  += KFIELD(fld);
+      FIELDD(fld) += KFIELDD(fld);
+    }
+  }
+  a  += ka;
+  ad += kad;
+
+  t += dt;
+}
